73ee87981b6f370163f41969dbb2ecf914a70172
donna <donnamarijne+gh@gmail.com>
2026-05-22 09:28:16 +0100

    remove : indentation in python

--- init.lua
+++ init.lua
@@ -144,6 +144,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'python',
+  callback = function()
+    vim.opt_local.cinkeys = vim.opt_local.cinkeys:remove ':'
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
